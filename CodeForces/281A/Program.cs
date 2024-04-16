020772849b707ebbfaa2dc13e0f1bcb1fa2095ae
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-16 17:43:32 +0300

    Add files via upload

---
+++ CodeForces/281A/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _281A
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Word = Console.ReadLine().ToArray();
+            int ASCIICharacter = Convert.ToInt32(Word[0]);
+            if (ASCIICharacter >= 97 && ASCIICharacter <= 122)
+            {
+                ASCIICharacter -= 32;
+                char NewCharacter = Convert.ToChar(ASCIICharacter);
+
+                Word[0] = NewCharacter;
+
+            }
+
+            Console.WriteLine(new string(Word));
+
+
+
+        }
+    }
+}
