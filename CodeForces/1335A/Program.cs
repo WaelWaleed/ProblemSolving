1b5d6efa66b68b1006a748f403653ccbbe6fc005
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-18 01:35:22 +0300

    Add files via upload

---
+++ CodeForces/1335A/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1335A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int TestCases = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < TestCases; i++)
+            {
+                int Candies = int.Parse(Console.ReadLine());
+
+                int Result = (Candies - 1) / 2;
+                Console.WriteLine(Result);
+
+            }
+
+        }
+    }
+}
---
+++ CodeForces/996A/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _996A
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Money = int.Parse(Console.ReadLine());
+
+            int k = 0, Bills = 0;
+
+            while(Money > 0)
+            {
+                switch(Money >= 100 ? "Over100"
+                    : Money >= 20 ? "Over20"
+                    : Money >= 10 ? "Over10"
+                    : Money >= 5 ? "Over5"
+                    : Money >= 1 ? "Over1" : "NA")
+                {
+                    case "Over100":
+                        k = Money / 100;
+                        Money -= k * 100;
+                        Bills += k;
+                        break;
+                    case "Over20":
+                        k = Money / 20;
+                        Money -= k * 20;
+                        Bills += k;
+                        break;
+                    case "Over10":
+                        k = Money / 10;
+                        Money -= k * 10;
+                        Bills += k;
+                        break;
+                    case "Over5":
+                        k = Money / 5;
+                        Money -= k * 5;
+                        Bills += k;
+                        break;
+                    case "Over1":
+                        k = Money;
+                        Money -= k;
+                        Bills += k;
+                        break;
+
+                }
+
+            }
+
+            Console.WriteLine(Bills);
+
+        }
+    }
+}
