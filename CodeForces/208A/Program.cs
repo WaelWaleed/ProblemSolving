807b0c6566bab48a92c2393402f055ab534a5ec0
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-18 17:18:22 +0300

    Add files via upload

---
+++ CodeForces/208A/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _208A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine();
+
+            string x = Input.Replace("WUB", " ").Trim();
+            Console.WriteLine(x);
+
+        }
+    }
+}
