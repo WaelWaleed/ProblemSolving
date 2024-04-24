8ff50503b44c319947971d6215fb94a2331cd818
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-24 14:48:50 +0300

    Add files via upload

---
+++ CodeForces/363B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _363B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int length = int.Parse(input[0]), hole = int.Parse(input[1]), min = 1000000000, sum = 0 ;
+            int index = 0;
+            input = new string[length];
+            input = Console.ReadLine().Split();
+
+            for(int i = 0; i <= input.Length - hole; i++)
+            {
+                sum = 0;
+                for(int j = i; j < i + hole; j++)
+                {
+                    sum += int.Parse(input[j]);
+                }
+                if(min != Math.Min(min, sum))
+                {
+                    min = Math.Min(min, sum);
+                    index = i + 1;
+                }
+            }
+            Console.WriteLine($"{index}");
+
+        }
+    }
+}
---
+++ CodeForces/579A/Program.cs
@@ -0,0 +1,25 @@
+﻿/*************Not UNDERSTANDABLE***************/
+
+using System;
+
+namespace _579A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int count = 1, result = 0;
+            for(int i = 0; result <= input; i++)
+            {
+                if(result != input)
+                {
+                    count++;
+                    result ++;
+                    result = result * 2;
+                }
+            }
+            Console.Write($"{count}");
+        }
+    }
+}
