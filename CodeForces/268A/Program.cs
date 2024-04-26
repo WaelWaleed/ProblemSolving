d6afcaddfdc9b3ac92536b28d9d1efcbb8c33890
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-27 01:09:29 +0300

    Add files via upload

---
+++ CodeForces/268A/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _268A
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Teams = int.Parse(Console.ReadLine());
+
+            List<int> Home = new List<int>(), Guest = new List<int>();
+            int Result = 0;
+
+            for(int i = 0; i < Teams; i++)
+            {
+                string[] Costumes = Console.ReadLine().Split();
+                
+                Home.Add(int.Parse(Costumes[0]));
+                Guest.Add(int.Parse(Costumes[1]));
+
+            }
+
+            for(int i = 0; i < Teams; i++)
+            {
+                int HomeCostume = Home[i];
+                //int GuestCostume = Guest[0];
+
+                for(int j = 0; j < Teams; j++)
+                {
+                    if(i != j)
+                    {
+                        if(HomeCostume == Guest[j])
+                        {
+                            Result++;
+                        }
+                    }
+                }
+
+            }
+
+            Console.WriteLine(Result);
+
+        }
+    }
+}
