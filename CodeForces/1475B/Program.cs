b4396001e1894a1574b7d8b722599588fa5e8705
WaelWaleed <54860343+WaelWaleed@users.noreply.github.com>
2024-04-27 01:39:19 +0300

    Add files via upload

---
+++ CodeForces/1475B/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1475B
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int TestCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < TestCases; i++)
+            {
+                int YearNumber = int.Parse(Console.ReadLine());
+
+                if (YearNumber >= 2020)
+                {
+                    if (YearNumber % 2020 == 0)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        int x = YearNumber / 2020;
+                        int y = YearNumber % 2020;
+                        if (y <= x)
+                        {
+                            Console.WriteLine("YES");
+                        }
+                        else
+                        {
+                            Console.WriteLine("NO");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
